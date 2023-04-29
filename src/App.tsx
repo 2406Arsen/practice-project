b2c19243cd9f8d6b4d6c38f2e4145db6c302490a
Arsen <polkpolk356@gmail.com>
2023-04-29 21:00:11 +0400

    add color variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Component } from "components/Component";
+import { useTheme } from "providers/ThemeProvider";
+import { classNames } from "shared/lib/classNames/classNames";
 
 const App = () => {
+  const { theme } = useTheme()
   return (
-    <div className="App">
+    <div className={classNames('App', {}, [theme])}>
       hello world
+      <Component />
     </div>
   );
 }
 
 export default App;
+
