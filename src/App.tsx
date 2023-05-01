72ab86cf74ce19b6f85926d95986d11146353cbc
Edgar <edgar.voskanyan99@gmail.com>
2023-05-01 22:30:20 +0400

    ButtonIcon ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Component } from "components/Component";
 import { useTheme } from "providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
+import {
+  ButtonIcon,
+  ButtonSize,
+  ButtonTypes,
+} from "shared/ui/Buttons/ButtonIcon";
 
 const App = () => {
-  const { theme } = useTheme()
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className={classNames('App', {}, [theme])}>
-      hello world
+    <div className={classNames("App", {}, [theme])}>
+      <button onClick={toggleTheme}>toggle theme</button>
       <Component />
+      <ButtonIcon size={ButtonSize.LARGE} type={ButtonTypes.PRIMARY} disabled />
     </div>
   );
-}
+};
 
 export default App;
-
