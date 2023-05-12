41ff5554d388c0ca88212c45716ab79dd1c3995b
Siras <siras4518@gmail.com>
2023-05-12 13:31:27 +0400

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Component } from "components/Component";
 import { useTheme } from "providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import Avatars, { AvatarsSize } from "shared/ui/Avatars/Avatars";
 import {
   ButtonIcon,
   ButtonSize,
   ButtonTypes,
 } from "shared/ui/Buttons/ButtonIcon";
-import ImageRatio, { ImageRatios } from "shared/ui/ImageRatio/ImageRatio";
 import Checkbox from "shared/ui/Checkbox/Checkbox";
 import RadioButton from "shared/ui/RadioButton/RadioButton";
 import Stepper from "shared/ui/Stepper/Stepper";
@@ -20,17 +18,9 @@ const App = () => {
       <button onClick={toggleTheme}>toggle theme</button>
       <Component />
       <ButtonIcon size={ButtonSize.LARGE} type={ButtonTypes.PRIMARY} disabled />
-      <Avatars
-        size={AvatarsSize.LARGE}
-        source="https://images.pexels.com/photos/39866/entrepreneur-startup-start-up-man-39866.jpeg?cs=srgb&dl=pexels-pixabay-39866.jpg&fm=jpg"
-      />
-      <ImageRatio
-        ratio={ImageRatios.ONETOONE}
-        source="https://img.freepik.com/free-photo/beautiful-view-greenery-bridge-forest-perfect-background_181624-17827.jpg?w=2000"
-      />
-      <Switch disabled/>
-      <RadioButton disabled/>
-      <Checkbox disabled/>
+      <Switch />
+      <RadioButton />
+      <Checkbox />
       <Stepper size={ButtonSize.LARGE}/>
     </div>
   );
