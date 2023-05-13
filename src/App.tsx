d4ef35d9f46658d3a3f7faf68bc25c44dfc607af
LyovVopyan <vopyanlevon2007@gmail.com>
2023-05-13 19:24:58 +0400

    pop-up-and-buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import { Component } from "components/Component";
 import { useTheme } from "providers/ThemeProvider";
+import { useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
+import Button from "shared/ui/Button/button";
+import image from "../src/shared/ui/Popapp/Image Placeholder (Copy paste here).jpg"
 import {
   ButtonIcon,
   ButtonSize,
   ButtonTypes,
 } from "shared/ui/Buttons/ButtonIcon";
+import PopApp from "shared/ui/Popapp/Pop-up";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+  console.log(isOpen);
+  const options = ["Option 1", "Option 2", "Option 3"];
+  const handleOptionSelect = (option: string) => {
+    console.log(`Selected option: ${option}`);
+  };
   return (
     <div className={classNames("App", {}, [theme])}>
       <button onClick={toggleTheme}>toggle theme</button>
       <Component />
-      <ButtonIcon size={ButtonSize.LARGE} type={ButtonTypes.PRIMARY} disabled />
+      <ButtonIcon size={ButtonSize.LARGE} type={ButtonTypes.PRIMARY}/>
+      <Button mode="Primary" size="large" state="Default" type="button" onClick={handleOpen} >open Pop-up</Button>
+      {isOpen ? <PopApp action="remove-item" isOpen={isOpen} onClose={handleClose} image imageSize="large" /> : null}
     </div>
   );
 };
