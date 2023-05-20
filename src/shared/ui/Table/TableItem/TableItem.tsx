0acb8ff8a3d65c9261a0feb1ce7145593a0c527d
Arsen <polkpolk356@gmail.com>
2023-05-20 20:41:38 +0400

    table view logic change

--- src/shared/ui/Table/TableItem/TableItem.tsx
+++ src/shared/ui/Table/TableItem/TableItem.tsx
@@ -1,54 +1,61 @@
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'providers/ThemeProvider'
-import { useMemo,memo } from 'react'
+import { useMemo, memo } from 'react'
 import cls from './TableItem.module.scss'
+import RadioButton from 'shared/ui/RadioButton/RadioButton'
+import Switch from 'shared/ui/Switch/Switch'
 
-export interface TableProps{
+export interface TableProps {
     isCaption?: boolean,
     leftPart?: string,
     icon?: any,
     rightPart?: string | any,
+    rightPartType?: 'button' | 'switch'
 }
 
 
-const Table:React.FC<TableProps> = memo((props) => {
-    const{
+const Table: React.FC<TableProps> = memo((props) => {
+    const {
         isCaption,
         leftPart,
         rightPart,
         icon,
+        rightPartType
     } = props;
-    
-    const { theme }=useTheme()
-    const mods = useMemo(() => ({}),[])
+
+    const { theme } = useTheme()
+    const mods = useMemo(() => ({}), [])
 
 
-    return(
+    return (
         <div
             className={classNames(cls.Table, mods, [
                 cls[theme],
             ])}
-            >
+        >
             <div className={cls.LeftPart}>
                 {leftPart
-                    ? <img 
+                    ? <img
                         src={leftPart}
                         className={classNames(cls.TableImg, mods, [])}
-                      /> 
+                        alt='alt'
+                    />
                     : <span className={cls.MapIcon}>{icon}</span>
                 }
                 <div className={cls.LeftPartText}>
                     <p>Content is here</p>
-                    { isCaption 
+                    {isCaption
                         ? <span className={classNames(cls.Caption, mods, [
-                                    cls[theme],
-                                ])}
+                            cls[theme],
+                        ])}
                         >For coption</span>
                         : null
                     }
                 </div>
             </div>
             {rightPart}
+            {rightPartType === 'button' && <RadioButton />}
+            {rightPartType === 'switch' && <Switch />}
         </div>
     )
 });
