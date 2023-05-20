aad2f517ea7ef42b3614a58b6dc052062d8cd4a6
Edgar <edgar.voskanyan99@gmail.com>
2023-05-20 19:31:13 +0400

    ProgressBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Component } from "components/Component";
 import { useTheme } from "providers/ThemeProvider";
 import { classNames } from "shared/lib/classNames/classNames";
-import ProgressBar from "shared/ui/ProgressBar/ProgressBar";
+import { ProgressBar } from "shared/ui/ProgressBar/ProgressBar";
 import {
   ButtonIcon,
   ButtonSize,
@@ -9,16 +9,8 @@ import {
 } from "shared/ui/Buttons/ButtonIcon";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
   return (
-    <div className={classNames("App", {}, [theme])}>
-      {/* <button onClick={toggleTheme}>toggle theme</button>
-      <Component /> */}
-      {/* <ButtonIcon size={ButtonSize.LARGE} type={ButtonTypes.PRIMARY} disabled /> */}
-      <ProgressBar />
-      <ProgressBar />
-      <ProgressBar />
-      <ProgressBar />
+    <div>
       <ProgressBar />
     </div>
   );
--- src/shared/ui/ProgressBar/ProgressBar.tsx
+++ src/shared/ui/ProgressBar/ProgressBar.tsx
@@ -1,60 +1,35 @@
-import React, { useState, useMemo, FC } from "react";
-import { classNames } from "shared/lib/classNames/classNames";
-import cls from "./ProgressBar.module.scss";
+import React, { useState, useEffect } from "react";
+// import cls from "./ProgressBar.module.scss";
+import "./ProgressBar.css";
 
-// const ProgressBar = () => {
-//   const [progress, setProgress] = useState(0);
+export const ProgressBar = () => {
+  const [progress, setProgress] = useState(0);
 
-//   return (
-//     <div>
-//       <div className="progress-bar">
-//         <div
-//           className="progress-bar-fill"
-//           style={{ width: `${progress}%`, backgroundColor: "black" }}
-//         >
-//           Progress Bar
-//         </div>
-//       </div>
-//       <div progress-label></div>
-//     </div>
-//   );
-// };
-
-// export default ProgressBar;
-
-export enum ProgressBarFill {
-  fill1 = 10,
-}
-
-export enum ProgressBarMode {
-  DARK = "dark",
-  LIGHT = "light",
-}
-
-interface ProgressBarProps {
-  className?: string;
-  mode?: ProgressBarMode;
-  fill?: number;
-}
-
-const ProgressBar: FC<ProgressBarProps> = (props) => {
-  const {
-    className,
-    mode = ProgressBarMode.LIGHT,
-    fill = ProgressBarFill.fill1,
-  } = props;
-
-  const mods = useMemo(() => ({}), []);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((prevProgress) =>
+        prevProgress >= 100 ? 0 : prevProgress + 10
+      );
+    }, 1500);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   return (
-    <div
-      className={classNames(cls.ProgressBar, mods, [
-        className,
-        cls[mode],
-        cls[fill],
-      ])}
-    ></div>
+    <div className="container">
+      <div className="progress-bar">
+        <div
+          className="progress-bar-fill"
+          style={{ width: `${progress}%` }}
+        ></div>
+      </div>
+      <div
+        className="progress-label"
+        style={{ color: "var(--color-primary-base)" }}
+      >
+        {progress}%
+      </div>
+    </div>
   );
 };
-
-export default ProgressBar;
